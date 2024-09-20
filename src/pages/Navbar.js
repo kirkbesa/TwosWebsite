cec01053ec339bb072e4d3553d47160f6553d3a2
mgbrllc <134356417+mgbrllc@users.noreply.github.com>
2024-09-20 20:00:26 +0800

    Background and CSS

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './App.css';
+import logo from '../images/TwosLogo.png';
 
 function Nav() {
     return (
-        <nav>
-            <ul className="nav-list">
-                <li>
-                    <Link to="/" className="nav-link">Home</Link>
-                </li>
-                <li>
-                    <Link to="/Menu.js" className="nav-link">About</Link>
-                </li>
-                <li>
-                    <Link to="/Order" className="nav-link">Contact</Link>
-                </li>
-                <li>
-                    <Link to="/Login" className="nav-link">Counter</Link>
-                </li>
-            </ul>
-        </nav>
+      <nav>
+        <div className="nav-container">
+          <img src={logo} alt="Logo" className="logo" />
+          <ul className="nav-list">
+            <li>
+              <Link to="/" className="nav-link">Home</Link>
+            </li>
+            <li>
+              <Link to="/Menu" className="nav-link">About</Link>
+            </li>
+            <li>
+              <Link to="/Order" className="nav-link">Order</Link>
+            </li>
+            <li className="login-item">
+              <Link to="/Login" className="nav-link">Login</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     );
-}
+  }
 
 export default Nav;
--- src/pages/Order.js
+++ src/pages/Order.js
@@ -3,7 +3,7 @@
 export default function Order() {
     return (
         <div>
-
+            <h1>Order</h1>
         </div>
     );
 }
