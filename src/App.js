09589f3cc75a185565c64cb32950af4f5eed6ca0
mgbrllc <134356417+mgbrllc@users.noreply.github.com>
2024-10-12 19:02:44 +0800

    Finalized navigation bar, login and sign up pages, and imported necessary images for backgrounds
    Finalized navigation bar, login and sign up pages, and imported necessary images for backgrounds

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom';
 import { CartProvider } from './components/CartContext';
 import Home from './pages/Home'; 
 import Menu from './pages/Menu'; 
 import Checkout from './pages/Checkout'; 
 import Login from './pages/Login'; 
+import Signup from './pages/Signup'; 
+import Logo from './images/TwosLogo.png'; 
+
 
 function App() {
   return (
     <CartProvider>
     <Router>
       <nav>
+      <div className="nav-container">
+            <img src={Logo} alt="Logo" className='Logo' />
         <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/Menu">Menu</Link></li>
-          <li><Link to="/Checkout">Cart</Link></li>
-          <li><Link to="/Login">Login</Link></li>
+          <li><NavLink to="/" className={({ isActive }) => isActive ? 'active-link' : ''}>Home</NavLink></li>
+          <li><NavLink to="/Menu" className={({ isActive }) => isActive ? 'active-link' : ''}>Menu</NavLink></li>
+          <li><NavLink to="/Checkout" className={({ isActive }) => isActive ? 'active-link' : ''}>Cart</NavLink></li>
+          <li><NavLink to="/Login" className={({ isActive }) => isActive ? 'active-link' : ''}>Login</NavLink></li>
         </ul>
+        </div>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} /> 
         <Route path="/Menu" element={<Menu />} />
         <Route path="/Checkout" element={<Checkout />} /> 
         <Route path="/Login" element={<Login />} /> 
+        <Route path="/Signup" element={<Signup />} /> 
       </Routes>  
     </Router>
+    
     </CartProvider>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import React from 'react';
 const Home = () => {
 
   return (
-    <div>
+    <div id='homeContainer'>
       <h1>Home Page</h1>
     </div>
   ); 
