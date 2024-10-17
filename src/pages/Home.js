0cc26d82b416c56b3e4b3c9c5bfcf6c5e8715d10
mgbrllc <134356417+mgbrllc@users.noreply.github.com>
2024-10-17 15:34:08 +0800

    Home Page Update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,43 +1,75 @@
-import './Home.css'; // Keep your styles here
-import React, { useState } from 'react';
+import './Home.css'; 
 import Image1 from '../images/Image1.jpg'; 
 import Image2 from '../images/Image2.jpg';
 import Image3 from '../images/Image3.jpg';
 import Image4 from '../images/Image4.jpg';
 import Image5 from '../images/Image5.jpg';
+import marker from '../images/marker.png';
+import promoImg from '../images/promoImage1.jpg';
+
+import Carousel from 'react-bootstrap/Carousel';
 
 const Home = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const images = [Image1, Image2, Image3, Image4, Image5];
-
-  const nextSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-  };
-
-  const prevSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
-  };
-
   return (
     <div id='homeBGContainer'>
       <div id='homeContainer'>
         <div id='imageCarousel'>
-          <div className='carouselImages'>
-            <img src={images[currentIndex]} alt={`Dish ${currentIndex + 1}`} className="carousel-image"/>
-            <button className='homeBtn' id="prev" onClick={prevSlide}>❮</button>
-            <button className='homeBtn' id="next" onClick={nextSlide}>❯</button>  
-          </div>
+          <Carousel>
+            <Carousel.Item>
+              <img className="carouselImg d-block w-100" src={Image1} alt="First slide" />
+              <Carousel.Caption>
+                <h3>First slide</h3>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="carouselImg d-block w-100" src={Image2} alt="Second slide" />
+              <Carousel.Caption>
+                <h3>Second slide</h3>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="carouselImg d-block w-100" src={Image3} alt="Third slide" />
+              <Carousel.Caption>
+                <h3>Third slide</h3>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="carouselImg d-block w-100" src={Image4} alt="Fourth slide" />
+              <Carousel.Caption>
+                <h3>Fourth slide</h3>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="carouselImg d-block w-100" src={Image5} alt="Fifth slide" />
+              <Carousel.Caption>
+                <h3>Fifth slide</h3>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
         </div>
+        
         <div id="secondRow">
-            <div className='sRow' id='AboutUsContainer'>
-              <h2>About Us</h2>
-              <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+          <div className='sRow' id='AboutUsContainer'>
+            <div className='aboutUsDiv'>
+            <h2>About Us</h2>
+            <img className="markerCircle" src={marker} alt="circle"/>
             </div>
-            <div className='sRow' id='PromosContainer'>
-              <h2>Promos</h2>
-              <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            <p>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
+          </div>
+          <div className='sRow' id='PromosContainer'>
+            <div className='promosDiv'><h2>Promos</h2>
+            <img className="markerCircle" src={marker} alt="circle"/>
             </div>
+            <p>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            {/* <img className="promoImg" src={promoImg} alt="promos"/> */}
+          </div>
         </div>
       </div>
     </div>
