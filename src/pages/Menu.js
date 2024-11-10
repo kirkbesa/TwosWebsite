05c09369fbfacf3d7f8bcbde4013f50ba4d71a11
kirkbesa <kirk.besa@ciit.edu.ph>
2024-11-10 20:30:23 +0800

    turned cart to offcanvas

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -10,6 +10,8 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 const Menu = () => {
     const [toggleState, setToggleState] = useState(1);
@@ -75,19 +77,28 @@ const Menu = () => {
         }
     };  
       
-      // Fetch data for each category when component mounts
+    // Fetch data for each category when component mounts
     useEffect(() => {
         fetchProductsByCategory('burgers');
         fetchProductsByCategory('pastas');
         fetchProductsByCategory('ricemeals');
     }, []);
 
+
+
+
+    // CART SHOW HIDE
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
         
             <div className="menu-parent fade-in">
                 <Container expand="lg" id="MenuContainer">
                 <Row>
-                    <Col xs={12} lg={8}>
+                    <Col xs={12} md={12} lg={12}>
                     <div className="menu-container scrollable-container">
                         <div className="block-tabs">
                             {/* <button className={toggleState === 1 ? 'tabs active-tabs' : 'tabs'} onClick={() => toggleTab(1)}>Burgers</button>
@@ -97,8 +108,34 @@ const Menu = () => {
                             <Navbar collapseOnSelect expand="lg" className="menuNav">
                                 <Container className="nav-container">
                                     <Navbar.Toggle aria-controls="responsive-navbar-nav" className="tabs-categories-container">
-                                        <button className={categoryState === false ? 'tabs-categories-closed' : 'tabs-categories-open'} onClick={() => categoryState === false ? setCategoryState(true) : setCategoryState(false)}>Categories</button>
-                                        { categoryState === false ? <></> : <hr /> }
+                                        <button 
+                                            className={categoryState === false ? 'tabs-categories-closed' : 'tabs-categories-open'} 
+                                            onClick={() => categoryState === false ? setCategoryState(true) : setCategoryState(false)}>
+                                        
+                                        { categoryState === false ? 
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            width="30"
+                                            height="30"
+                                            fill="#fff"
+                                            viewBox="0 0 16 16"
+                                        >
+                                        <path fillRule="evenodd" d="M2 2h12a1 1 0 0 1 0 2H2a1 1 0 0 1 0-2zm0 6h12a1 1 0 0 1 0 2H2a1 1 0 0 1 0-2zm0 6h12a1 1 0 0 1 0 2H2a1 1 0 0 1 0-2z" />
+                                        </svg>
+
+                                        : 
+                                        <svg
+                                            onClick={handleClose}
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 24 24"
+                                            width="30"
+                                            height="30"
+                                            style={{ cursor: 'pointer', fill: 'red' }}
+                                        >
+                                        <path d="M18 6L6 18M6 6l12 12" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                                        </svg>
+                                        }
+                                        </button>
                                     </Navbar.Toggle>
                                     <Navbar.Collapse id="responsive-navbar-nav" variant="light">
                                         <Nav className="me-auto">
@@ -107,7 +144,9 @@ const Menu = () => {
                                             <button className={toggleState === 3 ? 'tabs active-tabs' : 'tabs'} onClick={() => toggleTab(3)}>Pasta</button>
                                             <button className={toggleState === 4 ? 'tabs active-tabs' : 'tabs'} onClick={() => toggleTab(4)}>Extras</button>
                                         </Nav>
+                                        
                                     </Navbar.Collapse>
+                                    <button className='tabs viewCartButton' onClick={handleShow}>View Cart</button>
                                 </Container>
                             </Navbar>
                         </div>
@@ -127,15 +166,32 @@ const Menu = () => {
                                 <ProductList products={products.extras} updateCart={updateCart} />
                             </div>
                         </div>
-                        
                     </div>
                     </Col>
 
-                    <Col xs={12} lg={4}>
+                    <Offcanvas className="menu-cart-container" show={show} onHide={handleClose} placement="end">
+                        <Offcanvas.Header>
+                            <svg
+                                onClick={handleClose}
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                width="30"
+                                height="30"
+                                style={{ cursor: 'pointer', fill: 'red' }}
+                            >
+                                <path d="M18 6L6 18M6 6l12 12" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                            </svg>
+                        </Offcanvas.Header>
+                        <Offcanvas.Body>
+                            <Cart cartItems={cartItems} />
+                        </Offcanvas.Body>
+                    </Offcanvas>
+
+                    {/* <Col xs={12} lg={4}>
                         <div className="menu-cart-container scrollable-container">
                             <Cart cartItems={cartItems} />
                         </div>
-                    </Col>
+                    </Col> */}
 
                 </Row>
                 </Container>
