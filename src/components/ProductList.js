c4b88cbaf577a54a2e49431d214d278a2a07ede3
ariiezona <115564021+ariiezona@users.noreply.github.com>
2024-11-11 00:40:49 +0800

    Cart bug fixing
    fixed cart items being piled onto one item, fixed price breaking

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -5,7 +5,6 @@ import '../pages/Menu.css';
 import '../App.css';
 
 const ProductList = ({ products, updateCart }) => {
-    console.log("Products in ProductList:", products);
     return (
         <div className="row row-cols-1 row-cols-md-2 g-4">
             {products && products.map(product => (
