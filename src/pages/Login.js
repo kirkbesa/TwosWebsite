346cc05e8cb614cecc8f68f56ce4d96fa3909a45
mgbrllc <134356417+mgbrllc@users.noreply.github.com>
2024-11-13 23:10:54 +0800

    CSS Adjustments

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,27 +5,29 @@ import logo from '../images/TwosLogo.png';
 
 function Login() {
   return (
-    <div className='login-page'>
-    <div className="login-container fade-in">
-      <form className="login-form">
-      <div id="logoContainer">
-      <img src={logo} alt="Logo" className='LogoLI' /></div>
-        <div className="form-group">
-          <label htmlFor="username">Username</label>
-          <input type="text" id="username" name="username" required />
+    <div className='base-content'>
+      <div className='login-page'>
+        <div className="login-container fade-in">
+          <form className="login-form">
+          <div id="logoContainer">
+          <img src={logo} alt="Logo" className='LogoLI' /></div>
+            <div className="form-group">
+              <label htmlFor="username">Username</label>
+              <input type="text" id="username" name="username" required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" name="password" required />
+            </div>
+            <div id="loginBtn">
+            <button className="login-btn" type="submit">Login</button>
+            </div>
+          </form>
+          <p>
+            Don't have an account yet? <Link to="/Signup" className="register-link">Register</Link>
+          </p>
         </div>
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" name="password" required />
-        </div>
-        <div id="loginBtn">
-        <button className="login-btn" type="submit">Login</button>
-        </div>
-      </form>
-      <p>
-        Don't have an account yet? <Link to="/Signup" className="register-link">Register</Link>
-      </p>
-    </div>
+      </div>
     </div>
   );
 }
